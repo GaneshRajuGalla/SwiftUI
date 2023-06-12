132291bbd9a9c6e20efb116daebfa70221aa2f78
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-13 04:49:33 +0530

    Add files via upload

---
+++ ToolBarTitleMenu/ToolBarTitleMenu/ToolBarTitleMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolBarTitleMenuApp.swift
+//  ToolBarTitleMenu
+//
+//  Created by Ganesh on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ToolBarTitleMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
