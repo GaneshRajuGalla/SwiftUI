6e180114e58f54352a463f0eae081165d2beedb0
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-15 01:48:00 +0530

    Add files via upload

---
+++ ControlGroupMenu/ControlGroupMenu/ControlGroupMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlGroupMenuApp.swift
+//  ControlGroupMenu
+//
+//  Created by Ganesh on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ControlGroupMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
