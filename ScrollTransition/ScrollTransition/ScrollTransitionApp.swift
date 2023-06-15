5434080e1ee2fb4958c65e39a3cfcd1a8f99e9e3
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-16 00:41:43 +0530

    Add files via upload

---
+++ ScrollTransition/ScrollTransition/ScrollTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollTransitionApp.swift
+//  ScrollTransition
+//
+//  Created by Ganesh on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
