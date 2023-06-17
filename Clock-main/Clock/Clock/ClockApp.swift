28d03b71accadfefb539b7d7f02427289ef98c70
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-18 01:25:21 +0530

    Add files via upload

---
+++ Clock-main/Clock/Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Ganesh on 13/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
