6eb6e2c5a2f7f73441c515f507822c6c694fe0c3
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-18 00:59:19 +0530

    Add files via upload

---
+++ AnimationIndicator-main/AnimationIndicator/AnimationIndicator/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  AnimationIndicator
+//
+//  Created by Ganesh on 10/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isAnimating = false
+    var body: some View {
+        Circle()
+            .trim(from: 0,to: 0.6)
+            .stroke(AngularGradient(gradient: Gradient(colors: [.blue,.green]), center: .center),style: StrokeStyle(lineWidth: 8,lineCap: .round,dash: [0.1,16],dashPhase: 8))
+            .frame(width: 48,height: 48)
+            .rotationEffect(Angle(degrees: isAnimating ? 360 : 0))
+            .onAppear(){
+                withAnimation(Animation.linear(duration: 1)
+                    .repeatForever(autoreverses: false)){
+                    isAnimating.toggle()
+                }
+            }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AnimationIndicator-main/README.md
@@ -0,0 +1,4 @@
+# AnimationIndicator
+
+https://github.com/GaneshRajuGalla/AnimationIndicator/assets/61533653/0fe2075e-1f46-47cc-ba1a-9d2b8e44b11a
+
