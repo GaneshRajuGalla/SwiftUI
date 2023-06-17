2ebbd0931c51db004da862f4f72a6ee1d5adbbdf
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-18 01:03:20 +0530

    Add files via upload

---
+++ SpinningIndicator-main/README.md
@@ -0,0 +1,5 @@
+# SpinningIndicator
+
+
+https://github.com/GaneshRajuGalla/SpinningIndicator/assets/61533653/9256a165-18c6-4f10-8447-c3941dc243d1
+
---
+++ SpinningIndicator-main/SpinningIndicator/SpinningIndicator/SpinningIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinningIndicatorApp.swift
+//  SpinningIndicator
+//
+//  Created by Ganesh on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SpinningIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
