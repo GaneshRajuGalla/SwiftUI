1e10ea8c72e31e942bfadabdf2d84895a69c4641
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-18 00:27:32 +0530

    Add files via upload

---
+++ MetalUIBG/MetalUIBG/MetalUIBGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalUIBGApp.swift
+//  MetalUIBG
+//
+//  Created by Ganesh on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MetalUIBGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
