ef04bc006ce11fa52e14ad73487b4fc3a830073a
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-20 01:41:29 +0530

    Add files via upload

---
+++ ScrollPaging/ScrollPaging/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  ScrollPaging
+//
+//  Created by Ganesh on 20/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack{
+                ForEach(0..<50, id: \.self){ index in
+                    Text("Page: \(index)")
+                        .font(.title)
+                        .foregroundStyle(.white)
+                        .frame(maxHeight: .infinity)
+                        .frame(width: UIScreen.main.bounds.width - 45)
+                        .background(LinearGradient(colors: [.color,.color1], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .cornerRadius(20)
+                        .padding(20)
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.paging)
+    }
+}
+
+#Preview {
+    //ContentView()
+    VerticalScroll()
+}
+
+
+struct VerticalScroll:View {
+    var body: some View {
+        ScrollView(.vertical){
+            VStack{
+                ForEach(0..<50,id: \.self){ index in
+                    Text("Page: \(index)")
+                        .font(.title)
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: UIScreen.main.bounds.height - 125)
+                        .background(LinearGradient(colors: [.color,.color1], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        
+                        .cornerRadius(20)
+                        .padding(20)
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.paging)
+    }
+}
---
+++ ScrollPaging/ScrollPaging/ScrollPagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollPagingApp.swift
+//  ScrollPaging
+//
+//  Created by Ganesh on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollPagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
