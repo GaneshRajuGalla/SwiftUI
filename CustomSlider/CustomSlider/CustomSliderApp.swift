429e080654a1e89ff2af75959714b8987fe5cf72
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-25 03:20:18 +0530

    Add files via upload

---
+++ CustomSlider/CustomSlider/CustomSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSliderApp.swift
+//  CustomSlider
+//
+//  Created by Ganesh on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
