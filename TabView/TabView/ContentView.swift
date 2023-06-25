9ff24aeca29c86e2d4ed7a8d239fc7e852265169
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-25 11:46:19 +0530

    Add files via upload

---
+++ TabView/TabView/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  TabView
+//
+//  Created by Ganesh on 25/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Home")
+                .font(.title)
+                .tabItem {
+                    Label("Home", systemImage: "homekit")
+                        .symbolEffect(.variableColor)
+                }
+                .badge(20)
+            Text("List")
+                .font(.title)
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .badge(40)
+            Text("Notifications")
+                .font(.title)
+                .tabItem {
+                    Label("Notifications", systemImage: "text.bubble")
+                }
+                .badge(60)
+            Text("Settings")
+                .font(.title)
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+                .badge(80)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabView/TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Ganesh on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
