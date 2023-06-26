cf5e13d99a0071a994098bd549ee9d57dda9d3ea
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-26 21:12:56 +0530

    Add files via upload

---
+++ Base64Image/Base64Image/Base64ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Base64ImageApp.swift
+//  Base64Image
+//
+//  Created by Ganesh on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Base64ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
