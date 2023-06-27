08d49fbb29411986574f2588d1b113ed72cbf2c1
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-28 02:29:49 +0530

    Add files via upload

---
+++ DotDivider/DotDivider/DotDividerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotDividerApp.swift
+//  DotDivider
+//
+//  Created by Ganesh on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DotDividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
