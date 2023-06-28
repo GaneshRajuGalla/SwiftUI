aee21ac63dc387d7ee4fdb373ce214b554f0212a
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-29 03:58:24 +0530

    Add files via upload

---
+++ ContentUnavailable/ContentUnavailable/ContentUnavailableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentUnavailableApp.swift
+//  ContentUnavailable
+//
+//  Created by Ganesh on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ContentUnavailableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentUnavailable/ContentUnavailable/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  ContentUnavailable
+//
+//  Created by Ganesh on 29/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var ContentType:Int = 0
+    var body: some View {
+        VStack{
+            Spacer()
+            Picker("Content", selection: $ContentType) {
+                Text("Search").tag(0)
+                Text("Mail").tag(1)
+                Text("Favourite").tag(2)
+                Text("Weather").tag(3)
+            }
+            .pickerStyle(.segmented)
+            switch ContentType{
+            case 0:
+                ContentUnavailableView.search
+            case 1:
+                ContentUnavailableView("No mails", systemImage: "tray.fill")
+            case 2:
+                ContentUnavailableView("No favourites",systemImage: "star")
+                    .symbolVariant(.slash)
+            case 3:
+              
+                ContentUnavailableView.init {
+                    Label("Weather", systemImage: "cloud.rain")
+                }description: {
+                    Text("No weather data recorded")
+                }actions: {
+                    Button(action: {}, label: {
+                        Label("Refresh",systemImage: "arrow.clockwise")
+                    })
+                }
+            default:
+                Text("Nothing")
+            }
+        }
+        .padding()
+       
+    }
+}
+
+#Preview {
+    ContentView()
+}
