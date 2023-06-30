0ec87f0e61af72da7fbcde04b601a80520633914
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-30 21:33:09 +0530

    Add files via upload

---
+++ ListBG/ListBG/ListBGApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListBGApp.swift
+//  ListBG
+//
+//  Created by Ganesh on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ListBGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
