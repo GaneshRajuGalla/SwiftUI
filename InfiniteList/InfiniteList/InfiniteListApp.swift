8b2c245bdb7f83c11ecf49d96741a18fac76dedb
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-30 22:18:28 +0530

    Add files via upload

---
+++ InfiniteList/InfiniteList/InfiniteListApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteListApp.swift
+//  InfiniteList
+//
+//  Created by Ganesh on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
