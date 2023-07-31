308b1c7a709af0bc109caa54c22f028a647acfd7
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-08-01 03:43:18 +0530

    Add files via upload

---
+++ ZoomEffect/ZoomEffect/ZoomEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomEffectApp.swift
+//  ZoomEffect
+//
+//  Created by Ganesh on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ZoomEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
