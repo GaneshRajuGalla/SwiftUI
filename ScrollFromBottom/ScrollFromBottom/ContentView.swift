ce7a5db99aab5515004a54c3e755de817f2bf9bc
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-08-02 03:09:25 +0530

    Add files via upload

---
+++ ScrollFromBottom/ScrollFromBottom/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  ScrollFromBottom
+//
+//  Created by Ganesh on 02/08/23.
+//
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var items: [Int] = Array(0..<50)
+
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                ScrollViewReader { proxy in
+                    VStack(spacing: 10) {
+                        ForEach(items, id: \.self) { index in
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 100)
+                                .foregroundColor(Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1)))
+                                .id(index)
+                        }
+                    }
+                    .padding(12)
+                    .onChange(of: items.count) { _ in
+                        DispatchQueue.main.async {
+                            withAnimation {
+                                proxy.scrollTo(items.count - 1, anchor: .bottom)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationBarItems(trailing:
+                Button("Add Item") {
+                    items.append(items.count)
+                }
+            )
+            .navigationBarTitle(Text("ScrollFromBottom"))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrollFromBottom/ScrollFromBottom/ScrollFromBottomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollFromBottomApp.swift
+//  ScrollFromBottom
+//
+//  Created by Ganesh on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollFromBottomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
