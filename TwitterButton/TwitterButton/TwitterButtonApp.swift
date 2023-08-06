32df502abdc27eab92557d5eb21ca2cc7165ab14
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-08-07 03:07:58 +0530

    Add files via upload

---
+++ TwitterButton/TwitterButton/TwitterButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterButtonApp.swift
+//  TwitterButton
+//
+//  Created by Ganesh on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
