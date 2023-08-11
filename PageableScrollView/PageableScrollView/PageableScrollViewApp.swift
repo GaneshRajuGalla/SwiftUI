e553a68e3a960c501e0e0de429685c030cad6b41
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-08-11 23:46:06 +0530

    Add files via upload

---
+++ PageableScrollView/PageableScrollView/PageableScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageableScrollViewApp.swift
+//  PageableScrollView
+//
+//  Created by Ganesh on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PageableScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
