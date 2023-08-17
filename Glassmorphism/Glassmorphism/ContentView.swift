53178e8e1d855c2ba070478787ee445f470167d4
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-08-18 01:55:00 +0530

    Add files via upload

---
+++ Glassmorphism/Glassmorphism/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Glassmorphism
+//
+//  Created by Ganesh on 18/08/23.
+//
+
+import SwiftUI
+
+struct GlassmorphicStyle:ButtonStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .background(.ultraThinMaterial.opacity(0.7),in:RoundedRectangle(cornerRadius: 10))
+            .overlay(.white.opacity(0.5),in:RoundedRectangle(cornerRadius: 10).stroke(style: .init()))
+    }
+}
+
+extension ButtonStyle where Self == GlassmorphicStyle {
+    static var glassmorphism: GlassmorphicStyle {
+        .init()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ZStack(alignment: .center){
+            Image("apple")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            Button {
+                print("hello")
+            } label: {
+                Text("")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 200)
+            }
+            .padding(.horizontal,40)
+            .buttonStyle(.glassmorphism)
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Glassmorphism/Glassmorphism/GlassmorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphismApp.swift
+//  Glassmorphism
+//
+//  Created by Ganesh on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
