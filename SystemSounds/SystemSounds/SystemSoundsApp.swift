e197f7099738a080d3c825e80f647f889356e6d1
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-08-21 02:25:08 +0530

    Add files via upload

---
+++ SystemSounds/SystemSounds/SystemSoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemSoundsApp.swift
+//  SystemSounds
+//
+//  Created by Ganesh on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SystemSoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
