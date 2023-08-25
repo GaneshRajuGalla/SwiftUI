254f7ef8ae6161788f585d576a3c64747b246557
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-08-26 00:16:52 +0530

    Add files via upload

---
+++ icon/icon/iconApp.swift
@@ -0,0 +1,17 @@
+//
+//  iconApp.swift
+//  icon
+//
+//  Created by Ganesh on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct iconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
