4ff6f0a8ca1f54d05809031af3fccfde2f61e789
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-08-27 03:40:20 +0530

    Add files via upload

---
+++ RotationEffect/RotationEffect/RotationEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationEffectApp.swift
+//  RotationEffect
+//
+//  Created by Ganesh on 27/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RotationEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
