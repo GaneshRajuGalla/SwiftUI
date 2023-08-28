3889ecb00fb6abe5b0e7019123e78dac30b368ad
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-08-29 04:12:40 +0530

    Add files via upload

---
+++ TikTok/TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Ganesh on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
